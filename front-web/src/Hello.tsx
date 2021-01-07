17fe98593b8df11edd45689e807eb7402aa718f7
Renanvt <jonatan_rvs@hotmail.com>
2021-01-07 08:46:33 -0300

    undestanding the function useEffect()

--- front-web/src/Hello.tsx
+++ front-web/src/Hello.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+
 //Cria um tipo de dados
 type Props ={
     message: string;
 }
 function Hello({message} : Props){
+
+    //Faz uma ação quando o componente inicializar
+    //Um componente sofre várias reenderizações ao decorrer do tempo
+
+    useEffect(() => {
+        //Chamada para API para buscar os produtos
+        console.log('componente iniciou"');
+        
+    }, []);
     return (
         <h1>Hello {message}!</h1>
     )

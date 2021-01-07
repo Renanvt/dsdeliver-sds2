ebf5b1ea01eaa1eac7c0f2390d92727e9c4770d5
Renanvt <jonatan_rvs@hotmail.com>
2021-01-07 12:54:01 -0300

    Axios and fetch

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+    
+    useEffect(() => {
+     //Faz uma requisição no back-end
+     //npm install axios gerencia a requisição
+        fetchProducts()
+        //quando a requisição da sucesso
+        .then( response => setProducts(response.data))
+        .catch(error => console.log(error));
+        
+    }, []);
+
     return (
         <div className="orders-container">
            <StepsHeader />
+           <ProductsList products={products}/>
         </div>
     )
 }

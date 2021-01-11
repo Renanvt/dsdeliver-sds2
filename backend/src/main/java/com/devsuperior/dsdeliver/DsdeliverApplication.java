85894d1182f1d48db25a3594927f889350f3ec86
Renanvt <jonatan_rvs@hotmail.com>
2021-01-11 20:17:31 -0300

    adding Controller, RequestMapping and ResponseBody

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -3,10 +3,16 @@ package com.devsuperior.dsdeliver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DsdeliverApplication extends SpringBootServletInitializer{
 
+	@RequestMapping("/")
+	@ResponseBody
 	public static void main(String[] args) {
 		SpringApplication.run(DsdeliverApplication.class, args);
 	}

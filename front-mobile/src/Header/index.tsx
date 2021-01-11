5bdcb66c5be56d5c85d1cca0652431abf004a991
Renanvt <jonatan_rvs@hotmail.com>
2021-01-11 13:14:59 -0300

    index concluid

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,24 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+    {/*Redirecionar o usuário pra página de listagem de pedido */}
+    const navigation = useNavigation();
+        const handleOnPress = () => {
+            navigation.navigate('Home');
+    }
+{/*<TouchableWithoutFeedback>*Captura evento de click */}
+   
     return (
+
+        <TouchableWithoutFeedback onPress={handleOnPress}>
         <View style={styles.container}>
             <Image source={require('../assets/logo.png')} />
             <Text style={styles.text}>DS Delivery</Text>
         </View>
+        </TouchableWithoutFeedback>
     );
 }
 
---
+++ front-mobile/src/Routes.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import  { createStackNavigator } from '@react-navigation/stack';
+import Home from "./Home";
+import Orders from "./Orders";
+
+
+{/*Adicionar a pilha de navegação no app*/}
+const Stack = createStackNavigator();
+
+{/*<NavigationContainer> Vai encapsular todas as rotas da aplicação */}
+
+{/*<Stack.Navigator> Abstração da pilha de  rotas*/}
+
+{/*<Stack.Screen> Defini as rotas da app*/}
+function Routes() {
+    return (
+        <NavigationContainer>    
+            <Stack.Navigator 
+            headerMode="none"
+            screenOptions={{
+                cardStyle:{
+                    backgroundColor: '#FFF'
+                }
+            }}
+            >
+                <Stack.Screen name="Home" component={Home}></Stack.Screen>
+                <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes;

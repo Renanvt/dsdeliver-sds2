9cdf3628ce28b42787d1b45105fe382335e5bd62
Renanvt <jonatan_rvs@hotmail.com>
2021-01-11 20:09:22 -0300

    adding SpringBootServletInitializer

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -2,9 +2,10 @@ package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DsdeliverApplication {
+public class DsdeliverApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsdeliverApplication.class, args);

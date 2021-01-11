e361d81cf2eb68fa105c74be908032a2c8ef4049
Renanvt <jonatan_rvs@hotmail.com>
2021-01-11 20:36:18 -0300

    Trocando o profile

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -2,6 +2,7 @@ package com.devsuperior.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class DsdeliverApplication extends SpringBootServletInitializer{
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DsdeliverApplication.class, args);
 	}

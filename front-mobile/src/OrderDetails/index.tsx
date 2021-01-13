804f54cfff65f574251f2afcdd4c4673cd16affd
Renanvt <jonatan_rvs@hotmail.com>
2021-01-13 11:14:55 -0300

    integrating google maps and finishing front-mobile

--- front-mobile/src/OrderDetails/index.tsx
+++ front-mobile/src/OrderDetails/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Linking } from 'react-native';
 import Header from '../Header';
 import { Order } from '../Types';
 import OrderCard from '../OrderCard';
@@ -32,13 +32,17 @@ function OrderDetails({route}: Props) {
             Alert.alert(`Houve um erro ao confirmar o pedido ${order.id}`);
         })
     }
+
+    const handleStartNavigation = () => {
+        Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`)
+    }
    
     return (
         <>
         <Header />
             <View style={styles.container}>
               <OrderCard order={order}/>
-              <RectButton style={styles.button}>
+              <RectButton style={styles.button} onPress={handleStartNavigation}>
                   <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
               </RectButton>
               <RectButton style={styles.button} onPress={handleConfirmDelivery}>
